53b9f7a3c20843c364cfb12b445e36b36e9bbb62
stefan kessel <kessel.stefan1@gmail.com>
2022-01-10 18:44:03 +0100

    added webpack production mode

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,9 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  devtool: "cheap-module-source-map",
   entry: {
     popup: path.resolve("./src/popup/popup.tsx"),
     options: path.resolve("./src/options/options.tsx"),
@@ -43,6 +42,9 @@ module.exports = {
     },
   },
   plugins: [
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+    }),
     new CopyPlugin({
       patterns: [
         {
